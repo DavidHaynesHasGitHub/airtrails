d62279949466e7c9e106377c8b9c9c359270e2ca
David Haynes <DavidHaynesVictoria@gmail.com>
2018-04-06 08:32:54 -0700

    added user input for location

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,20 @@ var unirest = require('unirest');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let Result = unirest.get("https://trailapi-trailapi.p.mashape.com/?lat=49.28&limit=100&lon=-123.12&radius=25")
+  res.render('index', { title: 'Airtrail'});
+});
+
+router.post("/location", (req, res) => {
+  let location = req.body.location
+  console.log('location', location)
+  let Result = unirest.get("https://trailapi-trailapi.p.mashape.com/?limit=25&q[city_cont]=" + location + "&radius=25")
     .header("X-Mashape-Key", "qSCn2S9q6LmshglEQPriE2xmI6MSp1aAvSAjsnaTX0LGLXfmTZ")
     .header("Accept", "text/plain")
     .end(function (result) {
-      console.log(result.body);
-      res.send(Result.body);
-
+      console.log(result.status, result.headers, result.body);
     });
-  res.render('index', { title: 'Airtrail'});
 
-});
+  res.redirect('/', Result);
+})
 
 module.exports = router;
