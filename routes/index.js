dff0fdd6e33d320eb2e4d09adb381e2160b2ce49
David Haynes <DavidHaynesVictoria@gmail.com>
2018-04-05 22:08:10 -0700

    trailapi is receiving data from their api for vancouver. trying to send it to the front end in the response

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let Result = unirest.get("https://trailapi-trailapi.p.mashape.com/?lat=49.28&limit=100&lon=-123.12&radius=25")
+    .header("X-Mashape-Key", "qSCn2S9q6LmshglEQPriE2xmI6MSp1aAvSAjsnaTX0LGLXfmTZ")
+    .header("Accept", "text/plain")
+    .end(function (result) {
+      console.log(result.body);
+      res.send(Result.body);
 
+    });
+  res.render('index', { title: 'Airtrail'});
 
-
-  let location = getLocation();
-  console.log('the location is', location);
-  // res.render('index', { title: 'Airtrail', city: location});
 });
 
 module.exports = router;
