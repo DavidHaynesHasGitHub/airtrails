aaaa6d3456506e838c195c8bd04b887c2bd96ae2
David Haynes <DavidHaynesVictoria@gmail.com>
2018-04-06 20:02:34 -0700

    fixed linting errors

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,34 @@
-var express = require('express');
-var router = express.Router();
-var unirest = require('unirest');
+const express = require('express');
+const router = express.Router();
+const unirest = require('unirest');
+const bodyparser = require("body-parser");
+
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Airtrail'});
-});
-
-router.post("/location", (req, res) => {
-  let location = req.body.location
-  console.log('location', location)
-  let Result = unirest.get("https://trailapi-trailapi.p.mashape.com/?limit=25&q[city_cont]=" + location + "&radius=25")
+  let result = unirest.get("https://trailapi-trailapi.p.mashape.com/?limit=25&q[city_cont]=Vancouver&radius=25")
     .header("X-Mashape-Key", "qSCn2S9q6LmshglEQPriE2xmI6MSp1aAvSAjsnaTX0LGLXfmTZ")
     .header("Accept", "text/plain")
     .end(function (result) {
       console.log(result.status, result.headers, result.body);
     });
+  res.send(result)
+  res.render('index', { title: 'Airtrail'});
+});
 
-  res.redirect('/', Result);
-})
+// router.post("/location", (req, res) => {
+//   let location = req.body.location;
+//   console.log('location', location);
+//   let Result = {};
+//   Result = unirest.get("https://trailapi-trailapi.p.mashape.com/?limit=25&q[city_cont]=" + location + "&radius=25")
+//     .header("X-Mashape-Key", "qSCn2S9q6LmshglEQPriE2xmI6MSp1aAvSAjsnaTX0LGLXfmTZ")
+//     .header("Accept", "text/plain")
+//     .end(function (result) {
+//       console.log(result.status, result.headers, result.body);
+//     });
+//
+//   res.redirect('/');
+// });
 
 module.exports = router;
